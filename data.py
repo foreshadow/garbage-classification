f93b2f48292bcfabc14424347205137607fd011b
Zhibo Wang <573679956@qq.com>
2020-01-13 17:03:56 +0000

    Use tf.Data

--- data.py
+++ data.py
@@ -1,40 +1,36 @@
 import glob
-import random
 
 import tensorflow as tf
-from skimage.io import imread
-from tensorflow.python.keras.utils import Sequence
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+label1 = sum([[i] * c for i, c, in enumerate([6, 8, 23, 3])], [])
 
 
-class Garbage(Sequence):
-    def __init__(self, root_dir, batch_size=32, shuffle=True):
-        self.root_dir = root_dir
-        self.batch_size = batch_size
-        self.shuffle = shuffle
-        list_ = []
-        for file in glob.glob(f'{root_dir}/train_data/*.txt'):
-            with open(file) as f:
-                name, label = f.readline().split(', ')
-            list_.append((str(name), int(label)))
-        self.list = list_
+def preprocess_image(path):
+    blob = tf.io.read_file(path)
+    image = tf.image.decode_image(blob)
+    image = tf.image.resize_with_pad(image, 256, 256)
+    image = tf.image.random_crop(image, (224, 224, 3))
+    return image
 
-    def __len__(self):
-        return len(self.list) // self.batch_size
 
-    def __getitem__(self, item):
-        images = []
-        labels = []
-        for name, label in self.list[item * self.batch_size: (item + 1) * self.batch_size]:
-            image = imread(f'{self.root_dir}/train_data/{name}')
-            h, w, c = image.shape
-            target = max(h, w)
-            image = tf.image.pad_to_bounding_box(image, (target - h) // 2, (target - w) // 2, target, target)
-            image = tf.image.resize(image, (256, 256))
-            image = tf.image.random_crop(image, (224, 224, 3))
-            images.append(image)
-            labels.append(label)
-        return images, tf.constant(labels)
+def Garbage(root_dir, batch_size=32, shuffle=True):
+    list_ = []
+    for file in glob.glob(f'{root_dir}/train_data/*.txt'):
+        with open(file) as f:
+            path, label = f.readline().split(', ')
+            path = f'{root_dir}/train_data/{path}'
+        list_.append((str(path), int(label)))
 
-    def on_epoch_end(self):
-        if self.shuffle:
-            random.shuffle(self.list)
+    paths, labels = zip(*list_)
+    path_ds = tf.data.Dataset.from_tensor_slices(list(paths))
+    image_ds = path_ds.map(preprocess_image, num_parallel_calls=AUTOTUNE)
+    label_ds = tf.data.Dataset.from_tensor_slices(tf.cast(labels, tf.int64))  # .map(lambda y: label1[y], label)
+    ds = tf.data.Dataset.zip((image_ds, label_ds))
+
+    if shuffle:
+        ds = ds.shuffle(buffer_size=len(list_))
+    ds = ds.batch(batch_size).prefetch(buffer_size=AUTOTUNE)
+
+    return ds
--- index.py
+++ index.py
@@ -1,8 +1,10 @@
 import os
+from datetime import datetime
 
 import tensorflow_hub as hub
 from tensorflow.python.keras import Sequential
-from tensorflow.python.keras.activations import softmax
+from tensorflow.python.keras.activations import relu, softmax
+from tensorflow.python.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
 from tensorflow.python.keras.layers import Dense
 from tensorflow.python.keras.losses import sparse_categorical_crossentropy
 from tensorflow.python.keras.metrics import sparse_categorical_accuracy
@@ -11,12 +13,24 @@ from tensorflow.python.keras.optimizer_v2.adam import Adam
 from data import Garbage
 
 model = Sequential([
-    hub.KerasLayer("https://tfhub.dev/google/imagenet/mobilenet_v2_100_224/classification/4", name='mobilenetv2'),
-    Dense(40, activation=softmax, name='fc'),
-], name='classifier')
-model.build([None, 224, 224, 3])  # Batch input shape.
+    hub.KerasLayer("https://tfhub.dev/google/imagenet/mobilenet_v2_100_224/classification/4", name='mobile_net_v2'),
+    Dense(256, activation=relu),
+    Dense(40, activation=softmax),
+])
+model.build((None, 224, 224, 3))
 model.summary()
 
-generator = Garbage(os.path.expanduser('~/garbage_classify'))
-model.compile(optimizer=Adam(), loss=sparse_categorical_crossentropy, metrics=[sparse_categorical_accuracy],)
-model.fit_generator(generator, epochs=20)
+dataset = Garbage(os.path.expanduser('~/garbage_classify'))
+model.compile(
+    optimizer=Adam(),
+    loss=sparse_categorical_crossentropy,
+    metrics=[sparse_categorical_accuracy],
+)
+model.fit_generator(dataset, epochs=100, class_weight=None, callbacks=[
+    ModelCheckpoint(os.path.join(
+        'checkpoints',
+        datetime.now().strftime('%m%d%H%M%S'),
+        'mobilenetv2-40-ep{epoch:02d}-train{sparse_categorical_accuracy:.4f}.ckpt'
+    )),
+    ReduceLROnPlateau(monitor='loss', patience=5, factor=.2, verbose=True),
+])
