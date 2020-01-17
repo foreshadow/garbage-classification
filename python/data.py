013028d17fd81b47f5af4405e45181a5864058e3
Zhibo Wang <573679956@qq.com>
2020-01-17 01:03:38 +0000

    Better training code and data preprocessing

--- python/data.py
+++ python/data.py
@@ -1,6 +1,6 @@
-import glob
 import os
 
+import numpy as np
 import tensorflow as tf
 
 AUTOTUNE = tf.data.experimental.AUTOTUNE
@@ -8,15 +8,23 @@ AUTOTUNE = tf.data.experimental.AUTOTUNE
 label1 = sum([[i] * c for i, c, in enumerate([6, 8, 23, 3])], [])
 
 
-def preprocess_image(path):
+def preprocess_image(path, is_test=False):
     blob = tf.io.read_file(path)
     image = tf.image.decode_image(blob)
     image = tf.image.resize_with_pad(image, 256, 256)
-    image = tf.image.random_crop(image, (224, 224, 3))
+    if is_test:
+        image = tf.image.crop_to_bounding_box(image, 16, 16, 224, 224)
+    else:
+        image = tf.image.random_crop(image, (224, 224, 3))
+        image = tf.image.random_flip_left_right(image)
+        image = tf.image.random_brightness(image, .05)
     return image
 
 
-def Garbage(list_file, batch_size=32, shuffle=False):
+def Garbage(list_file, batch_size=32, is_test=False):
+    shuffle = not is_test
+    return_weights = not is_test
+
     root_dir = os.path.dirname(list_file)
     list_ = []
     with open(list_file) as f:
@@ -27,12 +35,16 @@ def Garbage(list_file, batch_size=32, shuffle=False):
 
     paths, labels = zip(*list_)
     path_ds = tf.data.Dataset.from_tensor_slices(list(paths))
-    image_ds = path_ds.map(preprocess_image, num_parallel_calls=AUTOTUNE)
+    image_ds = path_ds.map(partial(preprocess_image, is_test=is_test), num_parallel_calls=AUTOTUNE)
     label_ds = tf.data.Dataset.from_tensor_slices(tf.cast(labels, tf.int64))  # .map(lambda y: label1[y], label)
     ds = tf.data.Dataset.zip((image_ds, label_ds))
 
     if shuffle:
         ds = ds.shuffle(buffer_size=len(list_))
     ds = ds.batch(batch_size).prefetch(buffer_size=AUTOTUNE)
+
+    if return_weights:
+        freq = np.bincount(labels)
+        ds = ds, freq / freq.sum()
 
     return ds
--- python/index.py
+++ python/index.py
@@ -2,36 +2,37 @@ import os
 from datetime import datetime
 
 import tensorflow_hub as hub
+from tensorflow.keras.optimizers import Adam
 from tensorflow.python.keras import Sequential
-from tensorflow.python.keras.activations import relu, softmax
-from tensorflow.python.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
+from tensorflow.python.keras.activations import softmax
+from tensorflow.python.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, TensorBoard
 from tensorflow.python.keras.layers import Dense
 from tensorflow.python.keras.losses import sparse_categorical_crossentropy
 from tensorflow.python.keras.metrics import sparse_categorical_accuracy
-from tensorflow.keras.optimizers import Adam
 
 from data import Garbage
 
 model = Sequential([
-    hub.KerasLayer("https://tfhub.dev/google/imagenet/mobilenet_v2_100_224/classification/4", name='mobile_net_v2'),
-    Dense(256, activation=relu),
+    hub.KerasLayer("https://tfhub.dev/google/imagenet/mobilenet_v2_100_224/feature_vector/4", name='mobile_net_v2',
+        trainable=True, arguments=dict(batch_norm_momentum=0.997)),
     Dense(40, activation=softmax),
 ])
 model.build((None, 224, 224, 3))
 model.summary()
 
-train = Garbage(os.path.expanduser('~/garbage_classify/train.lst'), shuffle=True)
-val = Garbage(os.path.expanduser('~/garbage_classify/val.lst'))
+train, class_weight = Garbage(os.path.expanduser('~/garbage_classify/train.lst'), batch_size=64)
+val = Garbage(os.path.expanduser('~/garbage_classify/val.lst'), batch_size=256, is_test=True)
 model.compile(
     optimizer=Adam(),
     loss=sparse_categorical_crossentropy,
     metrics=[sparse_categorical_accuracy],
 )
-model.fit(train, epochs=300, class_weight=None, validation_data=val, callbacks=[
+model.fit(train, epochs=100, class_weight=class_weight, validation_data=val, callbacks=[
     ModelCheckpoint(os.path.join(
         'checkpoints',
         datetime.now().strftime('%m%d%H%M%S'),
         'mobilenetv2-40-ep{epoch:02d}-train{sparse_categorical_accuracy:.4f}-val{val_sparse_categorical_accuracy:.4f}.ckpt'
     )),
     ReduceLROnPlateau(monitor='loss', patience=5, factor=.2, verbose=True),
+    TensorBoard(),
 ])
